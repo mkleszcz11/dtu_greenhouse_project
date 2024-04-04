d6368f88df1dfc13470c08857e4d25379b4cb2cb
Kevin-Knudsen <s214376@dtu.dk>
2024-04-04 10:32:20 +0200

    Added JSON compatibility to the LORA setup

--- controller_esp32_communication/controller_esp32_communication.ino
+++ controller_esp32_communication/controller_esp32_communication.ino
@@ -1,12 +1,26 @@
 #include "lora.h"
 
+JsonDocument sensor_info;
+JsonDocument control_info;
+
 void setup() {
   Serial.begin(57600);
   lora_setup();
 }
 
 void loop() {
-  lora_transmit("ABC"); // Message is hex encoded
-  String message = lora_receive();
-  Serial.println("Received message: " + message);
+  // Create a quick json rpc for testing
+  sensor_info["humidity"] = 4;
+  sensor_info["temperature"] = 5;
+
+  // Transmit and recieve Lora signals
+  lora_transmit(&sensor_info);
+  lora_receive(&control_info);
+
+  //Print the transmited and recieved signals
+  Serial.println("Sent the sensor info:");
+  serializeJson(sensor_info, Serial); Serial.println();
+
+  Serial.println("Recieved the control info:");
+  serializeJson(control_info, Serial); Serial.println();
 }
--- esp32_cloud_communication/lora.h
+++ esp32_cloud_communication/lora.h
@@ -2,9 +2,10 @@
 #define LORA_H
 
 #include <Arduino.h>
+#include <ArduinoJson.h>
 
 void lora_setup();
-void lora_transmit(String message);
-String lora_receive();
+void lora_transmit(JsonDocument * sensor_info);
+void lora_receive(JsonDocument * control_info);
 
 #endif
